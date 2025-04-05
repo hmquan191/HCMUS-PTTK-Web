27d8e43fa33ddd75767c33674fc9883cc2762b18
HMQ <hmquan191@gmail.com>
2025-04-05 18:03:02 +0700

    add theme.js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,12 @@ import App from './App'
 import CssBaseline from '@mui/material/CssBaseline'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 
-// import theme from './theme'
+import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <CssVarsProvider>
-    {/* <CssBaseline /> */}
+  <CssVarsProvider theme={theme}>
+    <CssBaseline/>
     <App />
   </CssVarsProvider>
   // </React.StrictMode>
